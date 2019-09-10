cd5cb1f499460dd100a4d43319c0dc691e9101ee
AndyHur1 <hurby@ssfs.prg>
2019-09-10 15:08:23 -0400

    1.1 Exercise

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Printing {
+    public  static void main(String[] args) {
+
+        System.out.println("2:44pm");
+        System.out.println("9/10/2019");
+        System.out.println("It is a great\ntime to be\na computer programmer!");
+        System.out.print("It is a great");
+        System.out.print("time to be a");
+        System.out.println("computer programmer!");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"? Or between a \" and a \"? ");
+        System.out.println("One is what we see when we're typing our program.");
+        System.out.println("The other is what appears on the \"console.\"");
+        System.out.print(" \\");
+        System.out.println("/");
+        System.out.println("\\\\//");
+
+
+    }
 }
