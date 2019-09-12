a49098e50561303a4d67993e9aabd651eb09a39b
AndyHur1 <hurby@ssfs.prg>
2019-09-12 08:45:12 -0400

    1.1 Exercise

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -13,10 +13,18 @@ public class Printing {
         System.out.println("a ' and a \"? Or between a \" and a \"? ");
         System.out.println("One is what we see when we're typing our program.");
         System.out.println("The other is what appears on the \"console.\"");
-        System.out.print(" \\");
+        System.out.print("  \\");
         System.out.println("/");
-        System.out.println("\\\\//");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+        System.out.println("public class Hello {");
+        System.out.println("   Public static void main(String[] args) {");
+        System.out.println("       System.out.println(\"Hello, world!\");");
+        System.out.println("  }");
+        System.out.println("}");
 
-
-    }
+   }
 }
