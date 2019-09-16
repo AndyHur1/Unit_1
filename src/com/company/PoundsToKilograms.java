8d00ac164edccdcb41ae3c961436352d2f8d10c4
AndyHur1 <hurby@ssfs.prg>
2019-09-16 10:57:41 -0400

    1.3 Exercise

---
+++ src/com/company/PoundsToKilograms.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class PoundsToKilograms {
+    public static void main(String[] args)
+    {
+       double lbs;
+       double kgs;
+
+       lbs = 115.0;
+       kgs = lbs/2.205;
+       System.out.println((lbs)+" pounds = "+(kgs)+" kilograms");
+    }
+}
---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class RectangleArea {
+    public static void main(String[] args)
+    {
+        int length;
+        int width;
+        int area;
+
+        length = 14;
+        width = 6;
+        area = length*width;
+        System.out.println("Area = "+(area));
+
+    }
+}
